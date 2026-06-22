0712e8a5d5897964bb9bfd70ec86fe226bbb6a0f
YoussefHanna64-bit <JCBIOX200@GMAIL.COM>
2026-06-22 20:33:56 +0300

    feat: integrate Firebase and restructure app architecture

---
+++ lib/app/skycord_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SkyCordApp extends StatelessWidget {
+  const SkyCordApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "SkyCord",
+        home: Placeholder());
+  }
+}
